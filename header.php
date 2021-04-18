d396f0808797656a42d5b3ea1f4ef3f1fdc8fffb
c-proctor <79915136+c-proctor@users.noreply.github.com>
2021-04-18 17:10:33 +0800

    custom header

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 </head>
 
 <body>
-    <header class="container-fluid herobg">
+    <header class="container-fluid herobg"<?php if ( get_field(' headerbackgroundimage ') ) { echo 'style="background: url(' . get_field(' headerbackgroundimage ') . ')"'; } ?>>
         <nav class="navbar">
             <div class="container-fluid">
                 <div class="navbar-header">
@@ -77,7 +77,7 @@
 
 
         <div class="container">
-            <h1> Games Design For the Future </h1>
+            <h1><?php the_field('maintagline'); ?></h1>
         </div>
 
     </header>
